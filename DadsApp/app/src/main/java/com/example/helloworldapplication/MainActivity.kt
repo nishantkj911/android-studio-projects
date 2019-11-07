f225423ec99e768704699f7bd7c34a3e8a10a156
nishantkj911 <38178615+nishantkj911@users.noreply.github.com>
2019-11-07 20:12:47 +0530

    Another Sync

--- DadsApp/app/src/main/java/com/example/helloworldapplication/MainActivity.kt
+++ DadsApp/app/src/main/java/com/example/helloworldapplication/MainActivity.kt
@@ -39,7 +39,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 //        TODO("Adjust size of text in name text view and card number text view based on the length of string")
-        // adding fuckall code
     }
 
     private fun initCards() {
