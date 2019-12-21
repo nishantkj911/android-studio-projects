2cb0571d90d3cb1ca3c7f62f748bf29d488bdd22
nishantkj911 <38178615+nishantkj911@users.noreply.github.com>
2019-12-21 23:44:07 +0530

    Added 'are you sure' and verifying all fields if empty.

--- DadsApp/app/src/main/java/com/example/helloworldapplication/AddNewCardActivity.kt
+++ DadsApp/app/src/main/java/com/example/helloworldapplication/AddNewCardActivity.kt
@@ -1,8 +1,10 @@
 package com.example.helloworldapplication
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -27,35 +29,105 @@ class AddNewCardActivity : AppCompatActivity() {
         setOnClickListenersForValidTextBoxes()
         setOnClickListenerForButtons()
 
-//        TODO("Make a mechanism to give a message saying 'Are you sure' and then proceed")
+
 //        TODO("Make a undo message and mechanism")
 //        TODO("Show the already entered grid information while entering")
     }
 
     private fun setOnClickListenerForButtons() {
         submitButton.setOnClickListener {
-            val card: Card = Card(cardNameTextField.text.toString())
-            card.cardNumber = cardNumberTextField.text?.toString()
-            card.nameOnCard = nameOnCardTextField.text?.toString()
-            card.cvv = (cvvTextField.text)?.toString()!!.toInt()
+            val card = verifyIfFormatsAreCorrect()
 
-            // Valid Text views being updated using OnDateSetListeners
-            card.validFrom = dateFrom
-            card.validThru = dateThru
-            card.gridValues = hashMap
+            //A message saying 'Are you sure' and then proceed
+            val alertDialog = AlertDialog.Builder(this)
+                .setTitle("Are you sure you want to proceed?")
+                .setMessage("Please make sure the information is correct before proceeding.")
+                .setPositiveButton("YES") { dialog, _ ->
+                    MainActivity.cards!!.add(card!!)
+                    saveData(this)
+                    Toast.makeText(this, "New Card Successfully Added", Toast.LENGTH_LONG).show()
+                    dialog.dismiss()
+                    finish()
+                }
+                .setNegativeButton("NO") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
 
-            MainActivity.cards!!.add(card)
-            saveData(this)
-            Toast.makeText(this, "New Card Successfully Added", Toast.LENGTH_LONG).show()
-//            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-            // TODO("Change the above line of code to go back to the main activity instead of reopening it on top")
-            // TODO("Also change main activity to load the data in resume/restart method of the life cycle
+            if (card == null)
+                return@setOnClickListener
+            else
+                alertDialog.show()
         }
 
         addGridInfoButton.setOnClickListener {
             showGridInfoAddDialog()
         }
+    }
+
+    private fun verifyIfFormatsAreCorrect(): Card? {
+        var card: Card?
+        try {
+            Log.d(resources.getString(R.string.logtag), "Doing verification")
+            card = Card(cardNameTextField.text.toString())
+            card.cvv = (cvvTextField.text).toString().toInt()
+
+            card.cardNumber = if (cardNumberTextField.text.isEmpty()) {
+                Log.d(resources.getString(R.string.logtag), "throwing Exception")
+                throw KotlinNullPointerException()
+            } else
+                cardNumberTextField.text.toString()
+
+            card.nameOnCard = if (nameOnCardTextField.text.isEmpty()) {
+                Log.d(resources.getString(R.string.logtag), "throwing Exception")
+                throw KotlinNullPointerException()
+            } else
+                nameOnCardTextField.text.toString()
+        } catch (e: KotlinNullPointerException) {
+            AlertDialog.Builder(this)
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setTitle("Incomplete Card Details")
+                .setMessage("Please make sure all the details are filled.")
+                .create()
+                .show()
+
+            card = null
+        } catch (e: NumberFormatException) {
+            AlertDialog.Builder(this)
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setTitle("Invalid CVV")
+                .setMessage("Please make the format for CVV is proper")
+                .create()
+                .show()
+            card = null
+        }
+
+        if (card == null)
+            return null
+
+        card.gridValues = hashMap
+
+        // Valid Text views being updated using OnDateSetListeners
+        if (dateThru == null || dateFrom == null) {
+            card = null
+            AlertDialog.Builder(this)
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setTitle("Invalid Dates")
+                .setMessage("Please Select some date to proceed")
+                .create()
+                .show()
+        } else {
+            card.validThru = dateThru
+            card.validFrom = dateFrom
+        }
+
+        return card
     }
 
     private fun showGridInfoAddDialog() {
@@ -96,7 +168,6 @@ class AddNewCardActivity : AppCompatActivity() {
             }
 
         validFromTextView.setOnClickListener {
-            Log.d(resources.getString(R.string.logtag), "Valid from focus has been changed")
             val dp = DatePickerDialog(
                 this,
                 dateSetListenerValidFrom,
@@ -110,7 +181,6 @@ class AddNewCardActivity : AppCompatActivity() {
         }
 
         validThruTextView.setOnClickListener {
-            Log.d(resources.getString(R.string.logtag), "Valid thru focus has been changed")
             val dp = DatePickerDialog(
                 this,
                 dateSetListenerValidThru,
